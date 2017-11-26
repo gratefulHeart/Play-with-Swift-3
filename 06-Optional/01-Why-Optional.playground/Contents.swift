8039b574d015ee6aa43f23b1a8e03de4249408a2
JeffreyW2947 <wang@jwtherookie.com>
2017-11-25 21:18:34 -0500

    added a link to 6-01
    我将波波老师在慕课网上的手记《聊聊swift中的"??"》的链接添加到了6-01

--- 06-Optional/01-Why-Optional.playground/Contents.swift
+++ 06-Optional/01-Why-Optional.playground/Contents.swift
@@ -20,6 +20,8 @@ var errorCode:Int? = 404
  
 在swift3以上的xcode编译器中，直接print()一个可选型参数是会有Warning的，因为在打印nil的时候nil会隐式转换成一个字符串。如果想解决这个warning, 可以给print()方法设定一个默认值。当参数是一个可选型时，输出默认值
  详情：https://stackoverflow.com/questions/40691184/expression-implicitly-coerced-from-string-to-any
+ 
+ 转载波波老师的post: 对于nil聚合运算符，深入进去其实还有很多有意思的东西值得介绍，欢迎大家阅读我在慕课网的手记《聊聊swift中的"??"》。如果对于其中的一些东西不理解，可能看完第四季的运算符重载和协议相关，就能理解啦：） http://t.imooc.com/article/10325
 */
 
 print(errorCode ?? "errorCode value is nil")   //默认值为 "errorCode value is nil"
